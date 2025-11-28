ff6d0f2cdea48ad4e21c452292b07e13a5d8d480
huana23 <83851991+huana23@users.noreply.github.com>
2025-11-28 11:20:15 +0700

    Change Success BE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,12 @@
 import React from 'react';
-import { Fragment } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { publicRoutes } from './routes';
-import DefaultLayout from './components/Layout/DefaultLayout';
+import './App.css';
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Routes>
-                    {publicRoutes.map((route, index) => {                                        
-                        const Page = route.component;
-
-                        let Layout = DefaultLayout;
-                        if(route.layout) {
-                          Layout = route.layout
-                        }else if  (route.layout === null) {
-                          Layout = Fragment
-                        }
-                        return <Route key={index} path={route.path} element={<Layout><Page /></Layout>} />;
-                    })}
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+    </div>
+  );
 }
 
 export default App;
